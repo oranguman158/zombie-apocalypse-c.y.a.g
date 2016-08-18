05f3953d9daea4a49e39758a1a0d1f8e8cc3b252
kevinlima <kevon.lima@gmail.com>
2016-08-18 17:27:26 +0000

    master copy

--- application_controller.rb
+++ application_controller.rb
@@ -8,6 +8,21 @@ class ApplicationController < Sinatra::Base
   end
 
 	get '/1leave' do
-	erb :"1leave"
+	  erb :"1leave"
 	end
+
+  get '/1look_around' do
+    erb :"1look_around"
+  end
+
+   get '/2take_med' do
+    erb :"2take_meds"
+  end
+
+  get '/3keep_search' do
+    erb :"3keep_search"
+  end
+  get '/4what_get_bag' do
+    erb :"4what_get_bag"
+  end
 end
--- models/model.rb
+++ models/model.rb
@@ -1,1 +1,12 @@
 
+
+bag_contents=[
+  "car keys",
+  "water",
+  "food",
+  "car keys"
+  ]
+def bag_supplies
+user_get_item = bag_contents.sample
+  return user_get_item
+end
